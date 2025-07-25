21b464e78c159764bbc2119405ab3016db14218f
Siyaf Khan <siyafk019@gmail.com>
2025-07-25 13:33:06 +0500

    Comit Push

--- frontend/src/utils/BaseUrl.jsx
+++ frontend/src/utils/BaseUrl.jsx
@@ -1,3 +1,3 @@
-const baseUrl = import.meta.env.VITE_BACKEND_URL || "https://mern-auth-system-2s6a.onrender.com/api";
+const baseUrl = import.meta.env.VITE_BACKEND_URL || "https://mern-auth-system-apzd.onrender.com/api";
 
 export default baseUrl
