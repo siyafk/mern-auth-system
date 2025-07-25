11c0587a023360c7e4ed12c17b1682ef6ee21069
Siyaf Khan <siyafk019@gmail.com>
2025-07-25 06:52:12 +0500

    Fix backend url to avoid cors errors

--- frontend/src/utils/BaseUrl.jsx
+++ frontend/src/utils/BaseUrl.jsx
@@ -1,3 +1,3 @@
-const baseUrl = import.meta.env.VITE_BACKEND_URL || "https://mern-auth-system-0xss.onrender.com";
+const baseUrl = import.meta.env.VITE_BACKEND_URL || "https://mern-auth-system-2s6a.onrender.com";
 
 export default baseUrl
