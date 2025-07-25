69903a6f85d3f97fa746f055ac17914064c62267
Siyaf Khan <siyafk019@gmail.com>
2025-07-25 13:29:03 +0500

    Url Correction

--- frontend/src/utils/BaseUrl.jsx
+++ frontend/src/utils/BaseUrl.jsx
@@ -1,3 +1,3 @@
-const baseUrl = import.meta.env.VITE_BACKEND_URL || "https://mern-auth-system-2s6a.onrender.com";
+const baseUrl = import.meta.env.VITE_BACKEND_URL || "https://mern-auth-system-2s6a.onrender.com/api";
 
 export default baseUrl
